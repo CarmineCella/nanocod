48cb9221f64d1853bf3267daf139c61014145c54
Carmine Emanuele Cella <carmine.emanuele.cella@gmail.com>
2023-07-05 17:12:16 +0200

    removed feedback

--- nanocod.cpp
+++ nanocod.cpp
@@ -10,11 +10,13 @@
 using namespace std;
 
 int main (int argc, char* argv[]) {
-    cout << "nanocod, ver. 0.1" << endl << endl;
+    cout << "-----------------" << endl;
+    cout << "nanocod, ver. 0.1" << endl;
+    cout << "-----------------" << endl << endl;
 
     try {
-        if (argc != 9) {
-            throw runtime_error ("syntax: nancod input.wav output.wav nfft hop tstretch pshift denoise tau");
+        if (argc != 8) {
+            throw runtime_error ("syntax: nancod input.wav output.wav nfft hop tstretch pshift denoise");
             return 0;
         }
 
@@ -35,8 +37,6 @@ int main (int argc, char* argv[]) {
 
         float pshift = atof (argv[6]);
         float threshold = atof (argv[7]);
-        float tau = atof (argv[8]);
-        float feedback = pow (10.0, (-3.0 * (float) ohop / (tau * sr)));
 
         // error check
         if ((((~nfft + 1) & nfft) != nfft) || nfft < 2) { // check pow 2
@@ -51,9 +51,6 @@ int main (int argc, char* argv[]) {
         if (pshift <= 0) {
             throw runtime_error ("invalid pitch shift");
         }        
-        if (feedback < 0 || feedback >= 1) {
-            throw runtime_error ("invalid feedaback");
-        }                
 
         cout << "sr      : " << sr << endl;
         cout << "samples : " << samps << endl;
@@ -62,7 +59,6 @@ int main (int argc, char* argv[]) {
         cout << "stretch : " << tstretch << endl;
         cout << "shift   : " << pshift << endl;
         cout << "denoise : " << threshold << endl;
-        cout << "tau     : " << tau << " (feedback: " << feedback << ")" << endl;
         cout << "nfft    : " << nfft << endl;
         cout << "in olap : " << olap << endl;
         cout << "out olap: " << oolap << endl << endl;
@@ -73,7 +69,7 @@ int main (int argc, char* argv[]) {
         float* buffer = new float[samps * nchans + nfft];
         input.read (buffer, samps * nchans);
 
-        int osamps = (int) ((float)  samps * nchans * tstretch) + nfft + (int) (tau * sr / nchans);
+        int osamps = (int) ((float)  samps * nchans * tstretch) + nfft;
         float* obuffer = new float[osamps];
         memset (obuffer, 0, sizeof (float) * osamps);
 
@@ -85,9 +81,6 @@ int main (int argc, char* argv[]) {
         float* phi = new float[nfft];
         float* ophi = new float[nfft];
 
-        float* delay_amp = new float[nfft];
-        float* delay_freq = new float[nfft];
-
         float imax = 0;
         for (int i = 0; i < samps * nchans; ++i) {
             if (buffer[i] > imax) imax = buffer[i];
@@ -97,7 +90,6 @@ int main (int argc, char* argv[]) {
         //      for each window of input file:
         //          - compute fft
         //          - process fft data
-        //          - update phases
         //          - compute inverse fft
         cout << "processing..."; cout.flush ();
         for (int j = 0; j < nchans; ++j) {
@@ -105,8 +97,6 @@ int main (int argc, char* argv[]) {
             float opointer = 0;
             memset (phi, 0, sizeof (float) * nfft);
             memset (ophi, 0, sizeof (float) * nfft);
-            memset (delay_amp, 0, sizeof (float) * nfft);
-            memset (delay_freq, 0, sizeof (float) * nfft);
 
             while (pointer < osamps) {
                 memset (wksp, 0, sizeof (float) * 2 * nfft);
@@ -137,13 +127,6 @@ int main (int argc, char* argv[]) {
                         oamp[i] += amp[idx];
                         ofreq[i] = freq[idx] * pshift;
                     }
-                }
-
-                for (int i = 0; i < nfft; ++i) {
-                    oamp[i] += delay_amp[i] * feedback;
-                    ofreq[i] += delay_freq[i] * feedback;
-                    delay_amp[i] = oamp[i];
-                    delay_freq[i] = ofreq[i];
                 }
 
                 unconvert (oamp, ofreq, ophi, wksp, nfft, ohop, sr);
@@ -185,7 +168,6 @@ int main (int argc, char* argv[]) {
         delete [] ofreq;
         delete [] phi;
         delete [] ophi;
-
     } catch (exception& e) {
         cout << "error: " << e.what () << endl;
     } catch (...) {
